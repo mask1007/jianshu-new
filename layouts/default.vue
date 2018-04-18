fafbfdec36f745091473a9c0abbcf1b6d47676e5
mask1007 <1905180201@qq.com>
2018-04-18 15:00:31 +0800

    新建index.vue

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import BoostrapVue from 'bootstrap-vue'
+Vue.use(BoostrapVue)
+//首先引入bootstrap.css文件，加载bootstrap样式
+import 'bootstrap/dist/css/bootstrap.css'
+//引入bootstrap-vue.css文件
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+export default {
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>hello world</p>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                name:'首页'
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
