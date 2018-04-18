765fe7a57a724fa2c95d4ff4f8b72d6aee6dd92a
mask1007 <1905180201@qq.com>
2018-04-18 19:05:52 +0800

    简书登录注册页面，登录写了样式

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -14,7 +14,7 @@
                     <nuxt-link class="active" to="/sign-in">
                         登录
                     </nuxt-link>
-                    ·
+                    <b>·</b>
                     <nuxt-link to="/sign-up">
                         注册
                     </nuxt-link>
@@ -23,14 +23,21 @@
             <!--form表单-->
             <div class="js-form">
                 <form method="post">
-                    <input type="text" placeholder="手机号或邮箱">
-                    <i class="fa fa-user"></i>
-                    <input type="text" class="input-prepend" placeholder="密码">
-                    <i class="fa fa-lock"></i>
-                    <input class="remember" type="checkbox">
-                    <span>记住我</span>
+                    <div class="input-prepend">
+                        <input type="text" placeholder="手机号或邮箱">
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <div class="input-prepend">
+                        <input type="text" placeholder="密码">
+                        <i class="fa fa-lock"></i>
+                    </div>
+                    <div class="remember">
+                        <input type="checkbox">
+                        <span>记住我</span>
+                    </div>
+
                     <nuxt-link class="help" to="/">登录遇到问题</nuxt-link>
-                    <input type="button" class="btn btn-primary" value="登录">
+                    <button >登录</button>
                 </form>
             </div>
             <!--第三方的登录-->
@@ -51,7 +58,7 @@
                         <nuxt-link to="/">
                             <i class="fa fa-qq"></i>
                         </nuxt-link>
-                </li>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -62,12 +69,12 @@
     export default {
         //修改当前页面的默认模板为sign.vue
         //如果不写的话，默认是default.vue
-        layout:'sign',
+        layout: 'sign',
         //设置页面的标题以及描述、关键字等元信息
         head: {
             title: '登录-简书',
             meta: [
-                {charset:'utf-8'},
+                {charset: 'utf-8'},
                 {name: 'keywords', content: '登录页面'},
                 {name: 'description', content: '登录简书'},
                 {name: 'author', content: '孔露萍'}
